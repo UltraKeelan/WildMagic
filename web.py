ab8cdf2f2ea61de1b5e1145756914dc3e7c7e2d8
Lang <23107878+klang-uofi@users.noreply.github.com>
2018-12-05 16:30:14 -0600

    subpages and snark

--- web.py
+++ web.py
@@ -5,18 +5,29 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 application = app
 
-with io.open("effects.txt", 'r', encoding='utf8') as effects_source:
-    effects = effects_source.readlines()
-totaleffects = len(effects)
+with io.open("netlibram.txt", 'r', encoding='utf8') as netlibram_source:
+    netlibram = netlibram_source.readlines()
+netlibram_total = len(netlibram)
 
 @app.route("/")
 def index():
-    r = int(random.randrange(0, len(effects) - 1, 1))
-    return redirect(r, 303)
+    return render_template('index.html')
 
-@app.route("/<int:effectid>")
-def effect_lookup(effectid):
-    return effects[effectid]
+@app.route("/netlibram")
+def netlibram_start():
+    r = int(random.randrange(0, len(netlibram) - 1, 1))
+    return redirect("/netlibram/%d" % r, 303)
+
+@app.route("/netlibram/<int:effectid>")
+def netlibram_lookup(effectid):
+    if effectid > netlibram_total:
+        return "The maximum effect for Net Libram is %d" % netlibram_total
+    elif effectid == 0:
+        return "That's not how dice work, math genius."
+    elif effectid < 0:
+        return "How did you even get here?!"
+    else:
+        return netlibram[effectid]
 
 if __name__ == "__main__":
     app.run()
