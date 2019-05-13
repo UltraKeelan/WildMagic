787430f1a6d5d4fb42441cc1c7fe73158029ef0f
klang <23107878+klang-uofi@users.noreply.github.com>
2019-05-13 15:37:38 -0500

    Added table to result page and cleaned up comments

--- web.py
+++ web.py
@@ -3,35 +3,38 @@ import random
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-application = app # this is for Passenger, which expects application
+# Passenger is expecting the app you're trying to run to be called "application"
+# but I'm not sure if you can change Flask's assignment to just be "application"
+application = app
 
 # This will import any newline-delimited effects files passed to it as a text file
 def import_effects(effect_list):
-    # I was running into encoding errors when using regular ol' open so I used io's
+    # I was running into encoding errors when using regular ol' "open" so I
+    # switched to the  io library
     with io.open(effect_list, "r", encoding="utf8") as effects:
         return effects.readlines()
 
+# Paths for effects tables you'd like to import. I'm using relative paths but
+# absolute paths should (might?) be fine too
 nl_source = "nltable.txt"
 wm_source = "wmtable.txt"
 
+# Running the import function
 nl = import_effects(nl_source)
 wm = import_effects(wm_source)
 
 # Checks validity of inputs, and then returns a rendered template
-def effect_lookup(table, table_name, id):
-    table_length = len(table)
+def effect_lookup(table, name, id):
+    t_length = len(table)
     id -= 1
-    if id >= table_length:
-        # If you know of a better way to do this string, please
-        # for the love of god submit a pull request
-        return ("The maximum length for the {} table is {:,}!".format(table_name, table_length))
+    if id >= t_length:
+        return ("The maximum length for the {} table is {:,}!".format(name, t_length))
     elif id == -1:
         # This will happen if they pass a 0
-        # Grrrr this will break things I think...
         return "That's not how dice work!"
     elif id < -1:
-        # I haven't figured out if this is possible, but there may
-        # be a way for 
+        # I haven't figured out if this is possible, but there may be a way to
+        # get here somehow
         return "How did you even get here?"
     else:
         return table[id]
